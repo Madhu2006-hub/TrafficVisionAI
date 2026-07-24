cd6e11615216abd2e5873c3ec3e69c2757c1c403
Madhu2006-hub <madhuvarshinigarikipati@gmail.com>
2026-07-24 17:53:32 +0530

    new models folder

--- backend/models/traffic.py
+++ backend/models/traffic.py
@@ -1,12 +1,21 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 from database import Base
 
 
 class Traffic(Base):
+
     __tablename__ = "traffic"
 
     id = Column(Integer, primary_key=True, index=True)
+
     location = Column(String(100), nullable=False)
+
     vehicle_count = Column(Integer, nullable=False)
+
     congestion_level = Column(String(30), nullable=False)
+
     road_status = Column(String(30), nullable=False)
+
+    latitude = Column(Float, nullable=False)
+
+    longitude = Column(Float, nullable=False)
